10d5616dc109d849c5ffe0227beb7fc2b3692575
Frédéric Laberge <labf2804@dinf-0051-15b.DInf.fsci.usherbrooke.ca>
2019-12-02 11:23:39 -0500

    Complete the DataManager class to manage/load the data before the training process

--- Source/DataManager.py
+++ Source/DataManager.py
@@ -1,40 +1,48 @@
 # -*- coding: utf-8 -*-
-print "HELLO DOM"
 
 import pandas as pd
 import csv
+import math
 import numpy as np
 import os
 
-np.random.seed(0)
+class DataManager:
+    def __init__(self, nb_train, nb_test, normalisation=True, bias=True):
+        self.nb_train = nb_train
+        self.nb_test = nb_test
+        self.normalisation = normalisation
+        self.bias = bias
+    
+    def generer_donnees(self):
+        base_dir = os.path.join( os.path.dirname ( __file__), '..' )
+        csv = pd.read_csv(base_dir + "/Database/train.csv")
+        data = np.array(csv, dtype=np.int)
 
-################## Load Data
-base_dir = os.path.join( os.path.dirname ( __file__), '..' )
-csv = pd.read_csv(base_dir + "/Database/train.csv")
-#csv_test = pd.read_csv(base_dir + "/Database/test.csv")
-data = np.array(csv, dtype=np.int)
-#data_test = np.array(csv_train, dtype=np.int)
+        X_ = data[:,1:-1]
+        y_ = data[:,-1:] 
 
-X_ = data[:,1:-1]
-y_ = data[:,-1:] 
-#X_test = data_test[:,1:-1]
+        # Centrer et réduire les données (moyenne = 0, écart-type = 1)
+        if self.normalisation:
+            mean = np.mean(X_, axis=0)
+            std = np.std(X_, axis=0)
+            X_ = (X_ - mean) / std
+        
+        if self.bias:
+            X_ = augment(X_)
 
-################## Get attributes
-sample_size_train = data.shape[0]
-#sample_size_test = data_test.shape[0]
-num_features = X_.shape[1]
-features_name = list(csv.columns.values)
-num_classes = np.unique(y_).shape[0]
-print('Nombre de classes: ' + str(num_classes))
-print('Nom des features: ' + str(features_name))
-print('Nombre de features: ' + str(num_features))
-print('Nombre de donnees: ' + str(sample_size_train))
+        idx = np.random.permutation(len(X_Norm))
 
-# Centrer et réduire les données (moyenne = 0, écart-type = 1)
-mean = np.mean(X_, axis=0)
-std = np.std(X_, axis=0)
-print ("mean :", mean)
-print ("std :", std)
-X_Norm = (X_ - mean) / std
+        train_idx = idx[:self.nb_train]
+        test_idx = idx[-self.nb_test:]
 
-# Séparer les données localement en (train, valid, test)
+        X_train = X_[train_idx]
+        y_train = y_[train_idx]
+        X_test = X_[test_idx]
+        y_test = y_[test_idx]
+        return X_train, y_train, X_test, y_test
+
+def augment(X):
+    if len(X.shape) == 1:
+        return np.concatenate([X, [1.0]])
+    else:
+        return np.concatenate([X, np.ones((len(X), 1))], axis=1)
