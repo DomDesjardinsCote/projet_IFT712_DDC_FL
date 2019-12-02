7669972c22fa6fd66e217be9f92cf4735042d9d2
DomDesjardinsCote <57496756+DomDesjardinsCote@users.noreply.github.com>
2019-12-01 21:51:50 -0500

    Basic code for a Model.

---
+++ Source/Model.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""class Model:
+    code for a general model
+    def __init__(self):
+        pass
+
+    def train(self, x, t):
+        pass
+
+    def cross_validation(self, x, t):
+        pass
+
+    def prediction(self, x, t):
+        pass
+
+    def error(self, x, t):
+        pass
+"""
+import numpy as np
+
+class Model1:
+    def __init__(self):
+        """
+        TODO : Change the name of the model
+        Initialize the parameters of the model 1
+
+        """
+        print("Initialize model 1")
+
+    def train(self, x, t):
+        print("train Model 1")
+
+    def cross_validation(self, x, t):
+        print("Crossvalidation Model 1...")
+
+    def prediction(self, x, t):
+        print("Predict Model 1...")
+
+    def error(self, x, t):
+        print("Error Model 1...")
