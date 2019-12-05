fb60212658995453a6ab6260e4eae6a39a504a5a
DomDesjardinsCote <57496756+DomDesjardinsCote@users.noreply.github.com>
2019-12-05 15:39:43 -0500

    Adding the SVM model
    Adding the SVM model and have done some basic test.
    Need to do :
    - More complex test
    - Need to do cross validation (need to choose which kernel first)

--- Source/Model.py
+++ Source/Model.py
@@ -17,24 +17,66 @@
         pass
 """
 import numpy as np
-
-class Model1:
-    def __init__(self):
+from sklearn import tree            # Decision Tree Classifier
+from sklearn.svm import SVC         # SVM Classifier
+class ModelSVM:
+    def __init__(self, kernel='rbf', degree=3, coef0=0, verbose=False, classes=7):
         """
-        TODO : Change the name of the model
-        Initialize the parameters of the model 1
-
+        Initialize the parameters of the model SVM.
+        Kernel = Name of a kernel (linear, poly, rbf, sigmoid)
+        degree = Polynomial degree
+        coef0= coef0 of the linear model
+        verbose = See the trainning
+        Reference : https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
         """
-        print("Initialize model 1")
+        print("Initialize model SVM")
+        # Check if gamma='auto' is useful
+        self.kernel = kernel
+        self.mod = SVC(gamma='auto', kernel=kernel, degree=degree, coef0=coef0, verbose=verbose)
+        self.w = 0
+        self.w_0 = 0
+       # self.mod.classes_(classes)
 
     def train(self, x, t):
-        print("train Model 1")
+        """
+        Training the SVM model with the data.
+        :param x: A numpy array of the data
+        :param t: A numpy array of the classes
+        Comments : Remove self.w and self.w_0?
+        """
+        print("Training of the Model SVM")
+        self.mod.fit(x, t)
+        self.w = self.mod.dual_coef_
+        self.w_0 = self.mod.intercept_
 
     def cross_validation(self, x, t):
-        print("Crossvalidation Model 1...")
+        """
+        Do a cross validation on the hyperparameters.(Need to fine the hyper parameters for a cross-validation)
+        :param x: Dataset
+        :param t: Classes
+        """
+        print("TODO : Crossvalidation Model SVM...")
 
-    def prediction(self, x, t):
-        print("Predict Model 1...")
+    def prediction(self, x):
+        """
+        Predict the classe from the data.
+        :param x: One sample of data
+        :return:  The predict classes from the SVM model.
+        """
+        t = self.mod.predict(x)
+        return t
 
     def error(self, x, t):
-        print("Error Model 1...")
+        """
+        Compute the error between the predicted class and the class from data
+        if is not the same class : return 0
+        if is the same class   : return 1
+        :param x : One sample of data
+        :param t : Class of the sample data
+        """
+        predict = self.mod.predict(x.reshape(1, -1))
+        if t == predict:
+            return 0
+        else:
+            return 1
+
